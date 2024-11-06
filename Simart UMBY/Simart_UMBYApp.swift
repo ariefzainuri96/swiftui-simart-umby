8ffad5e65dde4dbfbf965c1c7818a3c8b3778460
skyshi-dev <arief@dihardjasoftware.com>
2024-11-06 10:09:54 +0700

    Initial Commit

---
+++ Simart UMBY/Simart_UMBYApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simart_UMBYApp.swift
+//  Simart UMBY
+//
+//  Created by фкшуа on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Simart_UMBYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
